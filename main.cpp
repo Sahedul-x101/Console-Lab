6cbc01fb6eb05c4a012d690d894d4b4999bd8d4e
Sahedul-x101 <sahedulhaquesrabon@gmail.com>
2025-11-29 18:30:49 +0600

    new all

---
+++ main.cpp
@@ -0,0 +1,25 @@
+/******************************************
+*   Console Lab   
+* 
+*   Console tools for simple uses.
+*   2025 :) 
+*   
+*
+*******************************************/
+
+#include <bits/stdc++.h>
+
+int main()
+{
+    while(true)
+    {
+        int option = 0;
+        std::cout << "Xx=========== Console Lab ===========xX" << std::endl;
+        std::cout << "1) Celsius to Fahrenheit & Fahrenheit to Celsius Convertor\n\n";
+        std::cout << "Xx===================================xX" << std::endl;
+
+        std::cin >> option;
+    }
+
+    return 0;
+}
